9295cffd55b5259c9aa1d7ef4156a22979aa75bd
Erik Drobne <erik.drobne@gmail.com>
2016-12-14 13:05:40 +0100

    Add domHelpers

---
+++ src/components/utility/domHelpers.js
@@ -0,0 +1,10 @@
+define('domHelpers', function() {
+    return {
+        findAncestor: findAncestor
+    }
+
+    function findAncestor (el, cls) {
+        while ((el = el.parentElement) && !el.classList.contains(cls));
+        return el;
+    }
+})
